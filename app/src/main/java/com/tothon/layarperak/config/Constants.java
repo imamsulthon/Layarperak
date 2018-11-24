20e42c35b9c39927647219b94d3c0a8b57f78091
Imam Sulthon <imamsulthon@yahoo.com>
2018-11-24 13:40:06 +0700

    empty api_key field

--- app/src/main/java/com/tothon/layarperak/config/Constants.java
+++ app/src/main/java/com/tothon/layarperak/config/Constants.java
@@ -2,8 +2,7 @@ package com.tothon.layarperak.config;
 
 public class Constants {
 
-    public static final String TMDB_API_KEY = "8947e379d40b3aa3345b64a447da95ff";
-    public static final String YOUTUBE_API_KEY = "AIzaSyBVCcxncgPSrDVABDTMtkJRf1WjydkM2mU";
-
+    public static final String TMDB_API_KEY = "";
+    public static final String YOUTUBE_API_KEY = "";
     public static final String YOUTUBE_BASE_URL = "https://img.youtube.com/vi/";
 }
